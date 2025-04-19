728e10b0a9e5b97457d68c13ccb69df175c3faec
IsheRayMajoni <ishemunyoromajoni@gmail.com>
2025-04-19 10:16:11 +0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ db_port          = ${DB_PORT:-5432}
 db_user          = ${DB_USER}
 db_password      = ${DB_PASSWORD}
 db_name          = ${DB_NAME}
-db_sslmode       = require 
+db_sslmode       = require
 addons_path      = /usr/lib/python3/dist-packages/odoo/addons
 EOF
 
