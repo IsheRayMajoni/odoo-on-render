7b1b6a0ade64d3f2de9ff9fca3b7fc58992ffa5d
IsheRayMajoni <ishemunyoromajoni@gmail.com>
2025-04-19 09:32:09 +0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# ----  Generate odoo.conf from environment variables  ----
 cat >/etc/odoo/odoo.conf <<EOF
 [options]
 xmlrpc_interface = 0.0.0.0
@@ -10,11 +9,8 @@ db_port          = ${DB_PORT:-5432}
 db_user          = ${DB_USER}
 db_password      = ${DB_PASSWORD}
 db_name          = ${DB_NAME}
+db_sslmode       = require
 addons_path      = /usr/lib/python3/dist-packages/odoo/addons
 EOF
 
-echo "Generated /etc/odoo/odoo.conf:"
-cat /etc/odoo/odoo.conf
-
-# ----  Hand off to the official Odoo entrypoint  ----
 exec "$@"
